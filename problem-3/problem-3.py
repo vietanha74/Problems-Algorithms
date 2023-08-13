ec27a8863cd71ca181cbb8e27f71540aaf593285
vietanha74 <vietanha74@gmail.com>
2023-08-13 07:56:33 +0700

    Update problem-3.py

--- problem-3/problem-3.py
+++ problem-3/problem-3.py
@@ -1,46 +1,24 @@
-def rearrange_digits(input_list):
-    result = []
+def mergesort_descending(items):
+
+    if len(items) <= 1:
+        return items
     
-    if len(input_list) == 0:
-        return result
+    mid = len(items) // 2
+    left = items[:mid]
+    right = items[mid:]
+    
+    left = mergesort_descending(left)
+    right = mergesort_descending(right)
+    
+    return merge_descending(left, right)
 
-    # Sort the input list using merge sort
-    sorted_list = merge_sort(input_list)
-
-    num1 = 0
-    num2 = 0
-
-    for i in range(len(sorted_list)):
-        if i % 2 == 0:
-            num1 = num1 * 10 + sorted_list[i]
-        else:
-            num2 = num2 * 10 + sorted_list[i]
-
-    result.append(num1)
-    result.append(num2)
-
-    return result
-
-def merge_sort(arr):
-    if len(arr) <= 1:
-        return arr
-
-    mid = len(arr) // 2
-    left = arr[:mid]
-    right = arr[mid:]
-
-    left = merge_sort(left)
-    right = merge_sort(right)
-
-    return merge(left, right)
-
-def merge(left, right):
+def merge_descending(left,right):
     merged = []
     left_index = 0
     right_index = 0
 
     while left_index < len(left) and right_index < len(right):
-        if left[left_index] > right[right_index]:
+        if left[left_index] >= right[right_index]:
             merged.append(left[left_index])
             left_index += 1
         else:
@@ -49,17 +27,28 @@ def merge(left, right):
 
     merged += left[left_index:]
     merged += right[right_index:]
-
+    
     return merged
 
-def test_function(test_case):
-    output = rearrange_digits(test_case[0])
-    solution = test_case[1]
-    if sum(output) == sum(solution):
-        print("Pass")
-    else:
-        print("Fail")
+def rearrange_digits(input_list):
+    # Sort the array in descending order
+    input_list = mergesort_descending(input_list)
+
+    # Initialize the two numbers
+    num1 = 0
+    num2 = 0
+
+    # Iterate over the sorted array and form the two numbers
+    for i in range(len(input_list)):
+        if i % 2 == 0:
+            num1 = num1 * 10 + input_list[i]
+        else:
+            num2 = num2 * 10 + input_list[i]
+
+    return [num1, num2]
+
+# Test cases
 
 
-# Test case
-test_function([[1, 2, 3, 4, 5], [531, 42]])
+input_list = [1, 2, 3, 4, 5, 6, 9]
+print(rearrange_digits(input_list))
