ed93d6afb0a890f58a7d3d0d1761330b0ac51d32
vietanha74 <vietanha74@gmail.com>
2023-09-01 22:57:59 +0700

    Update problem-1.py

--- problem-1/problem-1.py
+++ problem-1/problem-1.py
@@ -13,8 +13,8 @@ def sqrt(number):
     return end
 
 print("Pass" if (3 == sqrt(9)) else "Fail")
-print("Pass" if (0 == sqrt(0)) else "Fail")
+print("Fail" if (0 == sqrt(-9)) else "Pass")  # Edge case: negative number
+print("Fail" if (3.25 == sqrt(3.25)) else "Pass")  # Edge case: decimal number
 print("Pass" if (4 == sqrt(16)) else "Fail")
-print("Pass" if (1 == sqrt(1)) else "Fail")
 print("Pass" if (5 == sqrt(27)) else "Fail")
 
